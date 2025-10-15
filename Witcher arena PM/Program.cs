9aebd3811f1f751d2831854a127afba2cdce3dcc
patrik <smrzo@ASUS_PATRIK>
2025-10-15 15:56:13 +0200

    zakladne veci

--- Witcher arena PM/Program.cs
+++ Witcher arena PM/Program.cs
@@ -1,3 +1,7 @@
 ﻿System.Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 Console.BackgroundColor = ConsoleColor.DarkBlue;
 Console.Clear();
+Console.WriteLine($"Witcher Arena PM - Version {version}"); //zakladne veci nemenit
+                                                          
+
+
