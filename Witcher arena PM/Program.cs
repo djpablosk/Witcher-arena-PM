93dd3ef2dcf4b191de2ab1b26f8a84b12da650a8
patrik <smrzo@ASUS_PATRIK>
2025-10-15 16:33:53 +0200

    zaklad

--- Witcher arena PM/Program.cs
+++ Witcher arena PM/Program.cs
@@ -1,12 +1,16 @@
-﻿System.Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+﻿using System.Diagnostics.CodeAnalysis;
+
+System.Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 Console.BackgroundColor = ConsoleColor.DarkBlue;
 Console.Clear();
 Console.WriteLine($"Witcher Arena PM - Version {version}"); //zakladne veci nemenit
    
 Console.WriteLine("Vitaj v Witcher Arena ");
 
-string zobrazenie = "*";                                // celkove zobrazenie
-Console.WriteLine($""
+Console.WriteLine("Zadaj svoje meno: ");
+string playerName = Console.ReadLine();
+Console.WriteLine("Ahoj " + playerName + ", prave si vstupil do arény plnej príšer!");  //meno
+
 
 
 
