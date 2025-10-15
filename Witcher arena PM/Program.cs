5fb47f0fb88ea1bdc709671217a46ad56fb21d39
patrik <smrzo@ASUS_PATRIK>
2025-10-15 16:49:19 +0200

    upr

--- Witcher arena PM/Program.cs
+++ Witcher arena PM/Program.cs
@@ -3,9 +3,9 @@
 System.Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 Console.BackgroundColor = ConsoleColor.DarkBlue;
 Console.Clear();
-Console.WriteLine($"Witcher Arena PM - Version {version}"); //zakladne veci nemenit
+Console.WriteLine($"Witcher Arena PM - Version {version}"); //zakladne veci (nemenit)
    
-Console.WriteLine("Vitaj v Witcher Arena ");
+Console.WriteLine("Vitaj v Witcher Arena "); //welcome veta
 
 Console.WriteLine("Zadaj svoje meno: ");
 string playerName = Console.ReadLine();
